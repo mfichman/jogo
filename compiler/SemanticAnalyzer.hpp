1364e0440c55d77794a732b666a06f99f7c9c8c6
Matt Fichman <matt.fichman@gmail.com>
2011-08-28 01:55:49 -0700

    Renamed 'TypeChecker' to 'SemanticAnalyzer'

---
+++ compiler/SemanticAnalyzer.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2010 Matt Fichman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, APEXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */  
+
+#pragma once
+
+#include "Apollo.hpp"
+#include "TreeNode.hpp"
+#include "Environment.hpp"
+#include "Feature.hpp"
+#include <vector>
+#include <map>
+
+class SemanticAnalyzer : public TreeNode::Functor {
+public:
+	SemanticAnalyzer(Environment* environment);
+    typedef Pointer<SemanticAnalyzer> Ptr;
+
+private:
+    void operator()(Class* unit);
+    void operator()(Module* unit);
+    void operator()(Formal* formal);
+    void operator()(StringLiteral* expression);
+    void operator()(IntegerLiteral* expression);
+    void operator()(FloatLiteral* expression);
+    void operator()(BooleanLiteral* expression);
+    void operator()(Binary* expression);
+    void operator()(Unary* expression);
+    void operator()(Call* expression);
+    void operator()(Dispatch* expression);
+    void operator()(Construct* expression);
+    void operator()(Identifier* expression);
+    void operator()(Empty* expression);
+    void operator()(Block* statement);
+    void operator()(Simple* statement);
+    void operator()(Let* let);
+    void operator()(While* statement);
+    void operator()(Conditional* statement);
+    void operator()(Variable* statement);
+    void operator()(Return* statement);
+    void operator()(When* statement);
+    void operator()(Case* statement);
+    void operator()(Fork* statement);
+    void operator()(Yield* statement);
+    void operator()(Function* feature);
+    void operator()(Attribute* feature);
+    void operator()(Import* feature);
+    void operator()(Type* type);
+
+    Type* variable(String* name);
+    void variable(String* name, Type* type);
+    Function* function(String* name);
+    void function(String* name, Function* function);
+    Class* clazz(String* name);
+    void enter_scope();
+    void exit_scope();
+
+    Environment::Ptr environment_;
+    Stream::Ptr err_;
+    std::vector<std::map<String::Ptr, Type::Ptr> > variable_;
+    Module::Ptr module_;
+    Class::Ptr class_;
+    Function::Ptr scope_;
+    Type::Ptr return_; // Return value of the current block
+};
+
